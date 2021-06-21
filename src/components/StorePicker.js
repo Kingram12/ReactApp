2796c3537f566889535430213d2594545702c5a9
Kayla Ingram <kaylabsi@outlook.com>
2021-06-21 19:03:17 -0400

    events/refs

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,20 @@
 import React, { Fragment } from "react";
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+// In order to access THIS on a custom method - you must follow below syntax or constructor/super : setting this.method = this.method.bind(this*comp);
+
+  goToStore = (event) => {
+    event.preventDefault();
+  }
+
   render() {
     return (
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
             <h2>Please Enter A Store</h2>
-            <input type="text" required placeholder="Store Name"/>
+            <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()}/>
             <button type="submit">Visit Store</button>
         </form>
     )
