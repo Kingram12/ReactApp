6e3f4897dd66dd3a0fce0bafded8138a865e49b5
Kayla Ingram <kaylabsi@outlook.com>
2021-06-21 16:44:30 -0400

    main components made

---
+++ src/components/Inventory.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Inventory extends React.Component {
+    render() {
+      return (
+          <div className="Inventory">
+              Inventory
+          </div>
+      );
+    }
+  }
+
+
+
+
+
+
+
+
+export default Inventory;
